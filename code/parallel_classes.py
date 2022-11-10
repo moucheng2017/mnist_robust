3da6c3e4d9a60449468cbd46e9376b3c51954b5f
brangl <bojidar.rangelov.17@ucl.ac.uk>
2022-11-10 15:35:34 +0000

    adding the parallel classes

---
+++ code/parallel_classes.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from typing import Optional, List, Tuple, Union
+
+## arbitrary number of parallel MLP
+class MLP_para(nn.Module):
+    def __init__(self, hidden, output, num_exp):
+        self.hidden = hidden
+        self.num_exp = num_exp
+        self.output = output
+        super().__init__()
+
+        experts = []
+        for exp in range(self.num_exp):
+            experts.append(nn.Sequential(nn.Linear(self.hidden, self.output),
+                                         nn.BatchNorm1d(self.output), nn.ReLU()))
+        self.experts = experts
+
+    def forward(self, x):
+        outputs = []
+        for expert in self.experts:
+            outputs.append(expert(x))
+
+        outputs = torch.FloatTensor(outputs)
+        outputs = torch.sum(outputs, dim=0)
+
+        return outputs
+
+## arbitrary number of parallel 2D convolution
+class conv2D_para(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 dilation,
+                 kernel_size: int =3,
+                 num_experts: int = 2):
+        self.num_experts = num_experts
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.dilation = dilation
+        super().__init__()
+
+
+        experts = []
+        for expert in range(self.num_experts):
+            experts.append(nn.Sequential(
+                nn.Conv2d(self.in_channels, self.out_channels, 3, padding=dilation, stride=1,
+                          bias=False, dilation=dilation),
+                nn.BatchNorm2d(self.out_channels, affine=True),
+                nn.ReLU(inplace=True)
+            ))
+        self.experts = experts
+
+
+    def forward(self, x):
+        outputs = []
+        for expert in self.experts:
+            outputs.append(expert(x))
+
+        outputs = torch.FloatTensor(outputs)
+        outputs = torch.sum(outputs, dim=0)
+
+        return outputs
